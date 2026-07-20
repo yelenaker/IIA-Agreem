7e64cc92c824fd9eb9c11458da7c2462c42191a2
yelenaker <yelenaker@gmail.com>
2026-07-20 11:46:37 +0200

    admin

--- auth.js
+++ auth.js
@@ -44,7 +44,7 @@
 var GOOGLE_CLIENT_ID = "1026351516868-ckgto5ppumua6m2htqcoeuubid3jrnkb.apps.googleusercontent.com";
 
 /* Same Apps Script web app URL every page already talks to. */
-var API_BASE = "https://script.google.com/macros/s/AKfycbwrgoPNlVEGtdGamlAQDv8dsRWjH3u56cR7v9jiGEV1stJhH4mq_SmNLraV0fKE6VRU/exec";
+var API_BASE = "https://script.google.com/macros/s/AKfycby0Gcw_lb4r4cY1Pa4xhdclmt-tOfiAXNXQhmxgJB_YsR1DIMoV4nVD7OHIDiQftTnFig/exec";
 
 /* Scope needed to send mail as the signed-in user through the
    Gmail REST API. Requested via a SEPARATE OAuth token client
